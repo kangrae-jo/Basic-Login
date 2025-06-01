28814572a7d4b7b3ae84203e0c3acfcd3dfec637
kangrae <kangrae0501@gmail.com>
2025-06-01 22:22:22 +0900

    feat: webConfig 설정 추가

---
+++ src/main/java/kakao/config/WebConfig.java
@@ -0,0 +1,26 @@
+package kakao.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Value("${client.redirect-url}")
+    private String CLIENT_URL;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", CLIENT_URL)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+
+}
